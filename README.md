cde2d78cfd73461f4a2f1f05393a8a9434f92268
cuong9565 <cuongmanhle0905@gmail.com>
2025-08-14 10:39:10 +0700

    Fix Apriori

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Team_NCKH_2025
+# Thuật toán Apriori
+
+## Thời gian chạy
+|   file \ min_sup  |   0.85    |   0.9     |
+|-------------------|-----------|-----------|
+|chess.txt          |28739      |5245       |
--- apriori.cpp
+++ apriori.cpp
@@ -2,128 +2,126 @@
 #include <bits/stdc++.h>
 #define el cout << '\n'
 #define tm_opt ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define f0(i,n) for(int i=0; i<n; i++)
-#define f1(i,n) for(int i=1; i<=n; i++)
+#define f0(i,n) for(ll i=0; i<n; i++)
+#define f1(i,n) for(ll i=1; i<=n; i++)
 #define pb push_back
 #define all(a) a.begin(),a.end()
 using namespace std;
-using item_name = vector<string>;
+using namespace std::chrono;
 
-int n, min_sup, k = 0; // So dong, min_support, do dai item hien tai cua frequent
-vector<set<string>>transactions; // Danh sach tat ca giao dich
-vector<item_name>frequent; // Danh sach tap muc thuong xuyen
-vector<item_name>candidate; // Danh sach tap muc thanh vien (Candidate)
-map<item_name, int> mp_item; // Dung de tinh support
+int n, minsup;
+float min_sup = 0.9;
+vector<set<int>>transactions;
 
-// Ham nay de hien thi ten cua item
-string get_item_name(item_name name){
-    string str = "{";
-    if(name.size()>0){
-        for(string x: name) str += x+",";
-        str.pop_back();
-    }
-    str+="}";
-    return str;
-}
+map<int,int>C1;
+vector<int>L1;
 
-// Ham doc du lieu trong file
-void doc(){
-    cin >> n >> min_sup; cin.ignore(); // Doc n va min_sup
-    f0(i,n){ // Doc tat ca giao dich trong file
-        string str; getline(cin, str);
-        stringstream ss(str);
-        string s;
+set<vector<int>>C2;
+vector<vector<int>>L2;
 
-        set<string>transaction;
-        while(getline(ss,s,',')) if(s!=""){
-            transaction.insert(s);
-        }
-        transactions.pb(transaction);
-    }
+vector<vector<int>>Ck;
+vector<vector<int>>Lk;
 
-    // Chuyen transaction thanh mp_item
-    for(set<string> transaction: transactions){
-        for(string str: transaction){
-            mp_item[item_name{str}]++;
-        }
-    }
-
-    // Chuyen mp_item sang candidate va frequent
-    for(auto [u,v]: mp_item) if(v>=min_sup) {
-        frequent.pb(u);
-        candidate.pb(u);
-    }
-
-    // Tang do dai hien tai len 1
-    k++;
-}
-
-// Kiem tra k-1 phan tu dau co giong nhau khong
-bool check_same_k_1(item_name a, item_name b, item_name &c){
-    f0(i,k-1) if(a[i]!=b[i]) return false;
-    c = a;
-    c.pb(b[k-1]);
-    return true;
-}
-
-// Tim support cua item
-int cal_sup(item_name name){
-    if(mp_item.find(name)!=mp_item.end()) return mp_item[name];
-
+int cal_sup(vector<int>vt){
     int cnt = 0;
-    for(set<string>transaction : transactions){
-        int tmp = 0;
-        // Duyet qua tat ca cac ten nam trong name
-        for(string x: name){
-            if(transaction.find(x)!=transaction.end()) tmp++;
-            else break;
+    for(set<int>tr: transactions){
+        int have_item = 1;
+        for(int x: vt){
+            have_item = have_item && (tr.find(x)!=tr.end());
+            if(!have_item) break;
         }
-        cnt += (tmp==int(name.size()));
+        cnt += have_item;
     }
-    mp_item[name] = cnt;
-
     return cnt;
 }
 
-// Chay thuat toan apriori
-void apriori(){
-    int n = candidate.size();
-    vector<item_name>tmp;
+vector<int> intersection(vector<int>a, vector<int>b){
+    int len = a.size();
+    vector<int>c;
+    c.pb(a[0]);
+    c.pb(b[0]);
+    for(int i=1; i<len; i++)
+        if(a[i]!=b[i]) {
+            c.clear();
+            return c;
+        }
+        else c.pb(a[i]);
+    vector<int>itemset({a[0], b[0]});
+    if(C2.find(itemset)==C2.end()) c.clear();
+    return c;
+}
 
-    for(int i=0; i<n-1; i++){
-        for(int j=i+1; j<n; j++){
-            item_name new_item;
-            if(!check_same_k_1(candidate[i], candidate[j], new_item)) continue;
+void read_database(){
+    string str;
+    while(getline(cin, str)){
+        int num = 0;
+        set<int>tr;
+        for(char c: str){
+            if ('0'<=c && c<='9') num = num * 10 + (c - '0');
+            else if(num) {
+                C1[num]++;
+                tr.insert(num);
+                num=0;
+            }
+        }
+        if(num) {
+            C1[num]++;
+            tr.insert(num);
+            num=0;
+        }
+        transactions.pb(tr);
+        n++;
+    }
+    minsup = n * min_sup;
+}
 
-            if(cal_sup(new_item)>=min_sup){
-                frequent.pb(new_item);
-                tmp.pb(new_item);
+void solve(){
+    // Find L1
+    for(auto [u,v]: C1) if (v>=minsup) L1.pb(u);
+
+    // Find L2
+    int len_n = L1.size();
+    for(int i=0; i<len_n-1; i++){
+        for(int j=i+1; j<len_n; j++){
+            vector<int>items({L1[i], L1[j]});
+            int cnt = cal_sup(items);
+            if(cnt>=minsup){
+                C2.insert(items);
+                L2.pb(items);
             }
         }
     }
 
-    candidate = tmp;
-}
-
-// In ket qua
-void print_result(){
-    cout << "Danh sach tap muc thuong xuyen: ", el;
-    for(item_name x: frequent) cout << get_item_name(x) << " => " << mp_item[x] , el;
+    // Find Lk
+    Ck = L2;
+    while(Ck.size()){
+        vector<vector<int>>C;
+        int len = Ck.size();
+        for(int i=0; i<len-1; i++){
+            for(int j=i+1; j<len; j++){
+                vector<int>items = intersection(Ck[i], Ck[j]);
+                if(items.size() && cal_sup(items)>=minsup){
+                    C.pb(items);
+                    Lk.pb(items);
+                }
+            }
+        }
+        Ck = C;
+    }
 }
 
 signed main(){
     tm_opt;
     #ifdef demo
-    freopen("sinh.inp", "r", stdin);
-    freopen("code.ans", "w", stdout);
+    freopen("chess.txt", "r", stdin);
+//    freopen("code.ans", "w", stdout);
     #endif // demo
 
-    doc();
-    while(candidate.size()>0) {
-        apriori();
-        k++;
-    }
-    print_result();
+    auto start = high_resolution_clock::now();
+    read_database();
+    solve();
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << L1.size() + L2.size() + Lk.size(), el;
+    cout << duration.count() << " ms" , el;
 }
-
-
